c662598a06e3614b071947f91ef698332fca80ba
susumOyaji <sumitomo0210@gmail.com>
2021-11-24 17:45:44 +0900

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+//商品ID　m59704576093
+
 void main() => runApp(
       MaterialApp(
         home: MyApp(),
